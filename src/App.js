768f1e42eb7d374e768d71da7be59d17a756f54f
ChiefLopes <chieflopesekede@gmail.com>
2023-05-17 14:27:11 +0100

    my first react work

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import React from "react";
  function Apply() {
    const styles = {
      color: "blue",
-     fontSize: "18px",
+     fontSize: "35px",
      display: "flex",
-     justifyContent: "centre",
+     justifyContent: "center",
+     fontWeight: "bold",
    };
 
    return <div style={styles}>My first React project</div>;
